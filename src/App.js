63794f4b749f8f89bbf7fc4597a7cf5fe0452200
Emad Idris <emadidris@outlook.com>
2021-12-16 15:26:59 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Settings from './context/settings';
+import Auth from './context/auth';
+import ToDoContextComponent from './context/todo';
+import Header from './components/header/header';
+import ToDo from './components/todo/todo';
+import LogIn from './components/login/login';
+import SignUp from './components/signup/signup';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Auth>
+        <Settings>
+          <ToDoContextComponent>
+            <Header />
+            <Switch>
+              <Route exact path="/todo-app/">
+                <LogIn />
+                <ToDo />
+              </Route>
+              <Route path="/todo-app/signup">
+                <SignUp />
+              </Route>
+            </Switch>
+          </ToDoContextComponent>
+        </Settings>
+      </Auth>
+    </Router>
 
-export default App;
+  )
+}
